ebee3112321fbd29f0953082c762f4b7145abe20
j0kerjkl <sefersakir4@gmail.com>
2022-12-22 21:13:10 +0100

    Fix

--- src/api.js
+++ src/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 const SearchImages = async (term) => {
-    const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=636e1481b4f3c446d26b8eb6ebfe7127&tags=${term}&per_page=24&format=json&nojsoncallback=1`, {
-        headers: {
-            api_key: "636e1481b4f3c446d26b8eb6ebfe7127"
-        },
+    const response = await axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=636e1481b4f3c446d26b8eb6ebfe7127&tags=${term}&per_page=24&format=json&nojsoncallback=1`, {
+
         params: {
             tags: term,
             text: term
